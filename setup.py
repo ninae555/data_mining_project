3ae33fd8905dfb780b6f6c75ea874837f8698ebf
ninae555 <112419165+ninae555@users.noreply.github.com>
2023-03-20 15:13:58 -0400

    commit set up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+name ='data_mining_project',
+version= '0.0.1',
+authors = ['Nina', 'Cora', 'Meet', 'Qifan'],
+authors_email = 'ebensperger.nina@gmail.com',
+packages = find_packages(),
+install_requires=['pandas, numpy','seaborn','matplotlib','scikit-learn'],
+    
+)
